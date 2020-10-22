9a8aee545ca73a04ab127d17269e8990d9f659d1
kartik.m <homeSweet123>
2020-10-22 11:12:41 +0530

    Project Setup

---
+++ src/main/java/com/socgen/loanapprovalplatform/LoanapprovalplatformApplication.java
@@ -0,0 +1,13 @@
+package com.socgen.loanapprovalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanapprovalplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanapprovalplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socgen/loanapprovalplatform/LoanapprovalplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socgen.loanapprovalplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanapprovalplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
